63131c304c8d24339282cd5793e304a64138c310
Joona Nousiainen <joonan97@gmail.com>
2024-07-06 18:35:51 +0300

    fixup! fixup! Replace Router with HashRouter so page refresh doesn't give 404

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // App.js
 import React from "react";
-import { BrowserRouter as HashRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./Navbar";
 import PageHome from "./PageHome";
 import PageFestival from "./PageFestival";
